2b99540a15249979632dcb4a8bc2500659cd36ce
Nikita-Antonov <antonovenikita@gmail.com>
2020-09-28 09:23:56 -0400

    First Comit
    The first comit of the puzzle platfomer game
    -Basic Movement

---
+++ Puzzle Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+    public CharacterController characterController;
+
+    Vector3 moveDirection;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+    }
+
+    private void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+}
